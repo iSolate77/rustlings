bab5913019440832471b6627eefbcdb99c9f43e7
Mohamed Faris <mohd.faris07@gmail.com>
2023-03-27 23:25:53 +0300

    hashmaps

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,29 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    if input.contains(" ") {
+        input.trim().to_string()
+    } else {
+        input.to_string()
+    }
+
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_owned() + &" world!".to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    if input.contains("cars") {
+        input.replace("cars", "balloons")
+    } else {
+        input.to_string()
+    }
 }
 
 #[cfg(test)]
